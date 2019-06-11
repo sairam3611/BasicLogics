36d83b2babce9cd8d6b8470a3a76cad18215e2a2
sairam3611 <49762356+sairam3611@users.noreply.github.com>
2019-06-11 23:20:09 +0530

    Update ArmstringNumber.java

--- ArmstringNumber.java
+++ ArmstringNumber.java
@@ -1,1 +1,31 @@
-
+import java.util.*;
+public class ArmstrongNumber
+{ 
+    public static void checkArmstrong(int k){
+        int cubeSum=0;
+        int p=k;
+        while(k>0){
+            cubeSum+=((k%10)*(k%10)*(k%10));
+            k/=10;
+        }
+        if(p==cubeSum)
+          System.out.println("entered number"+p+ "is a ArmstrongNumber");
+        else
+          System.out.println("entered number"+p+ "is not a ArmstrongNumber");
+    }
+	public static void main(String[] args) {
+	
+	  Scanner sc= new Scanner(System.in);
+	  
+	  System.out.println("enter your Number :) ");
+	  
+	  int n=sc.nextInt();
+	  int cubeSum=0;
+	  if(n<0)
+	    checkArmstrong(n*(-1));
+	  else
+	    checkArmstrong(n);
+	  
+	  
+	}
+}
